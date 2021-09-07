c8088b81ce8b6fb9b4586d0b7cffbcbc35a297be
Aditya <aditya.r@email.com>
2021-09-07 08:21:13 +0530

    3.6.2

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:portfolio/CustomTheme.dart';
 import 'package:portfolio/models/Portfolio.dart';
 import 'package:portfolio/services/FirestoreService.dart';
@@ -11,6 +12,8 @@ import 'package:provider/provider.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle(statusBarColor: Colors.transparent));
   runApp(MyApp());
 }
 
